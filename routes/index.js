1a0955a3f6ceca69ba198379f854e1a3210a901b
rajeevksaini <B004319@federated.fds>
2017-03-15 23:56:30 -0400

    Test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure Deploy Testing App' });
+  res.render('index', { title: 'Azure Deploy Test App' });
 });
 
 module.exports = router;
