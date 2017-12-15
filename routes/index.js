a6fdee2b5f56d0fb8909d75aa34f89f46c059d1c
Rajeev <rajeevksaini@gmail.com>
2017-12-14 23:17:16 -0500

    testing slot

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure Deploy Test App' });
+  res.render('index', { title: 'Azure Deploy Test Slot' });
 });
 
 module.exports = router;
